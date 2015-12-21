1a78147dfaa90c482f8f765f2c8722498fd2c724
Andreas Ek <andreas@andreasek.se>
2015-12-21 16:55:52 +0100

    Namespace call fix

--- src/controller.php
+++ src/controller.php
@@ -6,7 +6,7 @@ class Controller {
 	function __construct() {
 
 		$model = new model();
-		add_action( 'template_include', [ $model, 'getPath' ] );
+		\add_action( 'template_include', [ $model, 'getPath' ] );
 		//add_filter( 'index_template', [ $model, 'getPath' ] );
 		//add_filter( 'page_template', [ $model, 'getPath' ] );
 		//add_filter( 'bp_template_include', [ $model, 'getPath' ] );
