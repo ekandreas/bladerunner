7a56b9a41d1817b3672c1ebf61e02988280b3581
Andreas Ek <andreas@andreasek.se>
2016-01-05 17:37:37 +0100

    Init converted to static function calls

--- src/init.php
+++ src/init.php
@@ -6,27 +6,26 @@ namespace Bladerunner;
  */
 class Init {
 
-	protected $cache;
-
 	function __construct() {
-		$this->cache = Template::cache();
-		add_action( 'admin_init', [ $this, 'check_writeable_upload' ] );
+		add_action( 'admin_init', '\Bladerunner\Init::check_writeable_upload' );
 	}
 
 	/**
 	 * Check if the cache folder exists and is writable and assigns admin notice if not
 	 * @return void
 	 */
-	function check_writeable_upload() {
+	static function check_writeable_upload() {
 
-		if( !file_exists( $this->cache ) ) {
-			add_action( 'admin_notices', [ $this, 'notice_create_cache' ] ); 
+		$cache = Template::cache();
+
+		if( !file_exists( $cache ) ) {
+			add_action( 'admin_notices', '\Bladerunner\Init::notice_create_cache' ); 
 		}
 		else {
 
-			$is_writable = @file_put_contents( $this->cache . '/.folder_writable', "true" );
+			$is_writable = @file_put_contents( $cache . '/.folder_writable', "true" );
 			if( !$is_writable ) {
-				add_action( 'admin_notices', [ $this, 'notice_writable_cache' ] ); 
+				add_action( 'admin_notices', '\Bladerunner\Init::notice_writable_cache' ); 
 			}
 
 		}
@@ -37,11 +36,12 @@ class Init {
 	 * Echo admin notice inside wp-admin if cache folder doesnt exist.
 	 * @return void
 	 */
-	function notice_create_cache() {
+	static function notice_create_cache() {
+		$cache = Template::cache();
         echo '<div class="error"> ';
         echo '<p><strong>Cache folder missing</strong></p>';
         echo '<p>Bladerunner needs a .cache -folder in uploads. Please create the folder and make it writable!</p>';
-        echo '<p>Eg, <i>mkdir ' . $this->cache . '</i></p>';
+        echo '<p>Eg, <i>mkdir ' . $cache . '</i></p>';
         echo '</div>'; 
     }
 
@@ -49,11 +49,12 @@ class Init {
 	 * Echo admin notice inside wp-admin if cache folder not writable.
 	 * @return void
 	 */
-	function notice_writable_cache() {
+	static function notice_writable_cache() {
+		$cache = Template::cache();
         echo '<div class="error"> ';
         echo '<p><strong>Cache not writable</strong></p>';
         echo '<p>Bladerunner cache folder .cache in uploads not writable. Please make the folder writable for your web server!</p>';
-        echo '<p>Eg, <i>chmod -R 777 ' . $this->cache . '</i></p>';
+        echo '<p>Eg, <i>chmod -R 777 ' . $cache . '</i></p>';
         echo '</div>'; 
     }
 
