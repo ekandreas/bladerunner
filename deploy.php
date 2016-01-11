5289afdb3881d69528d43eea7171ae55e7a823b6
Andreas Ek <andreas@andreasek.se>
2016-01-11 23:03:58 +0100

    Smaller deploy file needed

--- deploy.php
+++ deploy.php
@@ -1,7 +1,4 @@
 <?php
 date_default_timezone_set('Europe/Stockholm');
 
-include_once 'vendor/deployer/deployer/recipe/common.php';
 include_once 'vendor/ekandreas/testrunner/recipe.php';
-
-set('docker_host_name','test');
