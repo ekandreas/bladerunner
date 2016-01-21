7ec96554f82d5889d1ce53998d31b4617260ca3f
Andreas Ek <andreas@andreasek.se>
2016-01-21 09:14:09 +0100

    Cache test started

--- tests/cases/CacheTest.php
+++ tests/cases/CacheTest.php
@@ -6,4 +6,15 @@ class CacheTest extends WP_UnitTestCase
     {
         $this->assertContains('/.cache', EkAndreas\Bladerunner\Cache::path());
     }
+
+    public function testRemoveAllViews()
+    {
+        $this->assertNull(EkAndreas\Bladerunner\Cache::removeAllViews());
+    }
+
+    public function testConstructor()
+    {
+        $cache = new EkAndreas\Bladerunner\Cache();
+        $this->assertInstanceOf('EkAndreas\Bladerunner\Cache', $cache);
+    }
 }
