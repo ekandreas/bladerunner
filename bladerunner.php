3877c05ab7e5ec5465c7b21a72137076349d991a
Andreas Ek <andreas@andreasek.se>
2016-02-15 03:44:38 -0500

    Applied fixes from StyleCI

--- bladerunner.php
+++ bladerunner.php
@@ -27,19 +27,19 @@ add_action('save_post', 'Bladerunner\Cache::removeAllViews');
 add_action('admin_init', 'Bladerunner\Init::checkWriteableUpload');
 
 if (apply_filters('bladerunner/templates/handler', false)) {
-	$bladerunner_templates = new Bladerunner\Templates();
+    $bladerunner_templates = new Bladerunner\Templates();
     add_filter('template_include', [$bladerunner_templates, 'templateFilter'], 999);
     add_action('template_redirect', [$bladerunner_templates, 'addPageTemplateFilters']);
 }
 
-add_action('admin_menu', function(){
+add_action('admin_menu', function () {
     add_submenu_page(
         'tools.php',
         'Bladerunner',
         'Bladerunner',
         'manage_options',
         'bladerunner-admin-page',
-        'Bladerunner\Admin::view' );
+        'Bladerunner\Admin::view');
 });
  
 register_activation_hook(__FILE__, '\Bladerunner\init::createCacheDirectory');
