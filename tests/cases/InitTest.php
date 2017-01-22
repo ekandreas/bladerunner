54f1d2757bc9948f938aa050e03f4776edeb1c9d
Andreas Ek <andreas@elseif.se>
2017-01-22 18:31:02 +0100

    Starting with tests

--- tests/cases/InitTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class InitTest extends WP_UnitTestCase
-{
-    public function testActions()
-    {
-        //$this->assertSame(10, has_action('admin_init', '\Bladerunner\Init::checkWriteableUpload'));
-    }
-}
